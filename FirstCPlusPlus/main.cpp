e94c5cb0b4709ef3fcc6f58f1fc88be0e346fa19
tankedshoe <tankedshoe@gmail.com>
2018-01-23 08:35:26 -0700

    STARTED CPLUSSYPLUS!!

---
+++ FirstCPlusPlus/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FirstCPlusPlus
+//
+//  Created by Heaps, Dane on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World!" << endl;
+    return 0;
+}
